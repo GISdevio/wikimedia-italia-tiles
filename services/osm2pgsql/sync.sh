14992522ef79b377491a3fc4d434f840000701dc
Francesco Frassinelli <fraph24@gmail.com>
2024-09-01 11:13:20 +0200

    Improve import

--- services/osm2pgsql/sync.sh
+++ services/osm2pgsql/sync.sh
@@ -5,15 +5,13 @@ set -euo pipefail
 # check if data has been imported
 if [ $(psql -Atc "select exists (select * from pg_tables where schemaname='public' and tablename='planet_osm_ways')") == 'f' ]
 then
-    wget -O- -nv "$INITIAL_PBF" |
-        osm2pgsql \
-            --slim `# needed by osm2pgsql-replication` \
-            --multi-geometry \
-            --hstore \
-            --style openstreetmap-carto.style \
-            --tag-transform-script openstreetmap-carto.lua \
-            --number-processes 16 \
-            --input-reader=pbf -
+    osm2pgsql \
+        --slim `# needed by osm2pgsql-replication` \
+        --multi-geometry \
+        --hstore \
+        --style openstreetmap-carto.style \
+        --tag-transform-script openstreetmap-carto.lua \
+        --input-reader=pbf "$INITIAL_PBF"
 fi
 
 osm2pgsql-replication init --server "$REPLICATION_URL"
