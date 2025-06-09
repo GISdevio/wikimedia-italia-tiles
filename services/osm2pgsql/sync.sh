29baeb49b11efc6aef015a8554229cd9bd585c71
Francesco Frassinelli <fraph24@gmail.com>
2025-06-09 20:06:34 +0200

    Fix check

--- services/osm2pgsql/sync.sh
+++ services/osm2pgsql/sync.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # check if data has been imported
-if [ "$(psql -Atc 'select exists (select * from pg_tables where schemaname=\'public\' and tablename=\'planet_osm_ways\')')" == 'f' ]
+if [ "$(psql -Atc "select exists (select * from pg_tables where schemaname='public' and tablename='planet_osm_ways')")" == 'f' ]
 then
     osm2pgsql \
         --cache=20000 \
