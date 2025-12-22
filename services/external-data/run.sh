a08b60f8932235f85eddb43ba4908c77af37a292
Francesco Frassinelli <fraph24@gmail.com>
2025-12-22 17:56:18 +0100

    Remove obsolete external-data service

--- services/external-data/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-./scripts/get-external-data.py --cache --data /data -H $PGHOST -d $PGDATABASE -U $PGUSER -w $PGPASSWORD
-./scripts/indexes.py --concurrent --notexist | psql -v ON_ERROR_STOP=on
